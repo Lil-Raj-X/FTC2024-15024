04b95d46f02080c3188d964981d800ac508e67a5
Bhargava56789 <avvarubhargavakrishna@gmail.com>
2025-07-10 11:57:00 -0500

    Update MechanumDriveTest.java

--- FtcRobotController-10.1/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDriveTest.java
+++ FtcRobotController-10.1/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDriveTest.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
-public class MechanumDriveTest {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+@TeleOp(name = "Mechanum Drive Test", group = "TeleOp")
+public class MechanumDriveTest extends OpMode {
+    DcMotor leftBack;
+    @Override
+    public void init() {
+        leftBack = hardwareMap.get(DcMotor.class, "LeftBack");
+
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            leftBack.setPower(1);
+
+        }
+
+
+
+    }
 }
